a7bad4ecc5707484158ab2a7f705806278fe1da6
Victor <victorgpj25@gmail.com>
2022-09-17 16:40:59 -0300

    getTestsByDiscipline: route

--- src/routes/testRouter.ts
+++ src/routes/testRouter.ts
@@ -3,9 +3,11 @@ import express from 'express'
 import { postTest } from '../controllers/testController'
 import { validatePostTestReqBody } from '../middlewares/testMiddleware'
 import { verifyToken } from '../middlewares/authMiddleware'
+import { validateReqParamsId } from '../middlewares/reqParamsMiddleware'
 
 const testRouter = express.Router()
 
 testRouter.post('/tests', verifyToken, validatePostTestReqBody, postTest)
+testRouter.get('/tests/discipline/:disciplineId', verifyToken, validateReqParamsId, postTest)
 
 export default testRouter
