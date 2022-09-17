b19a118c02a0362fec8fbf786fa6b9f1d0960850
Victor <victorgpj25@gmail.com>
2022-09-17 18:32:31 -0300

    getTestsByDiscipline: repository

--- src/repositories/testRepository.ts
+++ src/repositories/testRepository.ts
@@ -7,3 +7,48 @@ export async function insert(testInsertData: testTypes.ItestInsertData) {
         data: testInsertData
     })
 }
+
+export async function findByDiscipline() {
+    const tests = await prisma.terms.findMany({
+        select: {
+            number: true,
+            disciplines: {
+                select: {
+                    name: true,
+                    teachersDisciplines: {
+                        select: {
+                            tests: { distinct: ['categoryId'],
+                                select: {
+                                    category: {
+                                        select: {
+                                            name: true,
+                                            tests: {
+                                                select: {
+                                                    id: true,
+                                                    name: true,
+                                                    pdfUrl: true,
+                                                    teacherDiscipline: {
+                                                        select: {
+                                                            teacher: {
+                                                                select: {
+                                                                    name: true
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+
+                                                }
+                                            }
+                                        }
+                                    }
+
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    })
+    return tests
+}
